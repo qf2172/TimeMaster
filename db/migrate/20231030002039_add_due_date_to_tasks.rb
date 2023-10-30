8b3d066d5e4593b5eaf98ef6a0876e7fa76483d8
Jiacheng Zhang <jz3658@columbia.edu>
2023-10-30 01:43:26 +0000

    make it better

--- db/migrate/20231030002039_add_due_date_to_tasks.rb
+++ db/migrate/20231030002039_add_due_date_to_tasks.rb
@@ -1,5 +1,7 @@
-class AddDueDateToTasks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tasks, :due_date, :date
+class CreateTasks < ActiveRecord::Migration[6.0]
+    def change
+      create_table :tasks do |t|
+        add_column :tasks, :due_date, :date
+      end
+    end
   end
-end
